584684c5887fa3d1713389c81c76b49ac1d2b589
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2010-11-08 17:11:08 +0000

    omnisocial update

--- lib/extensions/action_controller/base.rb
+++ lib/extensions/action_controller/base.rb
@@ -32,12 +32,12 @@ class ActionController::Base
   
   def current_user
     @current_user ||= if session[:user_id]
-      User.find(session[:user_id]) rescue nil
+      User.find(session[:user_id])
     elsif cookies[:remember_token]
-      User.find_by_remember_token(cookies[:remember_token]) rescue nil
-    else
-      nil
+      User.find_by_remember_token(cookies[:remember_token])
     end
+  rescue
+    logout!
   end
   
   def current_user?
